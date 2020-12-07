82b10903994157f1cb59b0e0abdbd561405aaa0b
Brandon Avant <avant.brandon@gmail.com>
2020-12-06 20:27:32 -0600

    Added unit tests

--- src/AdventOfCodeTemplate.Base/Exceptions/RuleValidationException.cs
+++ src/AdventOfCodeTemplate.Base/Exceptions/RuleValidationException.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AdventOfCodeTemplate.Base.Exceptions
 {
-    class RuleValidationException : Exception
+    public class RuleValidationException : Exception
     {
         public IRule BrokenRule { get; }
 
---
+++ src/AdventOfCodeTemplate.Tests/Base/RuleTests.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCodeTemplate.Base.Exceptions;
+using AdventOfCodeTemplate.Base.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCodeTemplate.Tests.Base
+{
+    public class RuleTests
+    {
+        [Fact]
+        public void AocEnvironmentVarMustBeSetRule_EmptyString_BreaksRuleAndShowsException()
+        {
+            // Arrange
+            var rule = new AocEnvironmentVarMustBeSetRule(string.Empty);
+            var classUsingRule = new ClassUsingRule();
+
+            // Assert
+            Assert.Throws<RuleValidationException>(() => classUsingRule.CheckRule(rule));
+        }
+
+        public class ClassUsingRule
+        {
+            public void CheckRule(IRule rule)
+            {
+                if (rule.IsBroken())
+                {
+                    throw new RuleValidationException(rule);
+                }
+            }
+        }
+    }
+}
